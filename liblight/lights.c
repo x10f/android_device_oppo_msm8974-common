edde02b8d3f44caf463e88daac929c39a52220dd
martincz <martincz@mokeedev.com>
2014-11-25 03:13:10 +0800

    mokee bringup
    Change-Id: I68029dd608d5c8e6c92911da0a30c0b8397a2c16

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2013 The MoKee OpenSource Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -455,6 +455,6 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
     .name = "Oppo Lights Module",
-    .author = "The CyanogenMod Project",
+    .author = "The MoKee OpenSource Project",
     .methods = &lights_module_methods,
 };
